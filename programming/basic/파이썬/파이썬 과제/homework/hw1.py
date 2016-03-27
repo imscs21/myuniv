d4c08b6b976c4d203120a952d23bda2c81b53bb3
se-hyun Hwang <imscs21@naver.com>
2016-03-27 19:33:15 +0900

    fix hw1.py

--- programming/basic/파이썬/파이썬 과제/homework/hw1.py
+++ programming/basic/파이썬/파이썬 과제/homework/hw1.py
@@ -1,10 +1,10 @@
-Grade_A = [90,100];
-Grade_B = [80,89];
-Grade_C = [70,79];
-Grade_D = [60,69];
-Grade_F = [0,59];
+Grade_A = [int(90),int(100)];
+Grade_B = [int(80),int(89)];
+Grade_C = [int(70),int(79)];
+Grade_D = [int(60),int(69)];
+Grade_F = [int(0),int(59)];
 
-def getGrade(score):
+def GetGrade(score):
     if(score>=Grade_A[0] and score <= Grade_A[1]):
         return "A";
     elif(score<Grade_A[0] and score >= Grade_B[0]):
@@ -17,19 +17,26 @@ def getGrade(score):
     else:
        return "F";
     
-def getQuestion(idx):
-    if(idx==0):
+def GetQuestion(idx):
+    if (idx==0):
         return "백분위 점수: ";
-    elif(idx == 1 ):
+    elif (idx == 1 ):
         return "더할래?(y/n)";
+    elif ( idx == 2 ):
+        return "1-가 프로그램 안녕";
+    elif ( idx == 3):
+        return "1-나 프로그램 안녕";
+    elif ( idx== 4 ):
+        return "학점:";
+		
     else: return "error";
 class CoreFunctionContainer:
-    def do(score):
+    def DoCoreFunction(score):
         loopControl0 = True;
         while loopControl0:
             yourscore=0;
             try:
-                tempValue1 = input(getQuestion(0));
+                tempValue1 = input(GetQuestion(0));
                 if(not tempValue1.isnumeric()):
                     continue;
                 else:
@@ -37,7 +44,7 @@ class CoreFunctionContainer:
                     if(yourscore>100 or yourscore<0):
                         continue;
                     else:
-                        print("학점:",getGrade(yourscore));
+                        print(GetQuestion(4),GetGrade(yourscore));
                         break;
                    
             except:continue;
@@ -45,18 +52,19 @@ class CoreFunctionContainer:
             
 def grade0():
     cfc = CoreFunctionContainer();
-    cfc.do();
-    print("1-가 프로그램 안녕");
+    cfc.DoCoreFunction();
+    del cfc;
+    print(GetQuestion(2));
     exit();
 def grade1():
     cfc = CoreFunctionContainer();
     loopControl1 = True;
     while loopControl1:
-        cfc.do();
+        cfc.DoCoreFunction();
         loopControl2 = True;
         while loopControl2:
         
-            tempReply = input(getQuestion(1));
+            tempReply = input(GetQuestion(1));
             tempReply = str(tempReply);
             if(tempReply == "n" or tempReply == "N"):
                 
@@ -77,8 +85,8 @@ def grade1():
         del loopControl2;
     del loopControl1;
 
-        
-    print("1-나 프로그램 안녕");
+    del cfc;   
+    print(GetQuestion(3));
     exit();
 def bigger(a,b) : 
     if a > b : 
@@ -132,7 +140,7 @@ def format_ok(f,m,b) :
    return True;
 
 def last_digit_ok(vals) : 
-    return 11 - ((2*int(vals[0])+3*int(vals[1])+4*int(vals[2])+5*int(vals[3])+6*int(vals[4])+7*int(vals[5])+8*int(vals[6])+9*int(vals[7])+2*int(vals[8])+3*int(vals[9])+4*int(vals[10])+5*int(vals[11])) % 11);
+    return (11 - ((2*int(vals[0])+3*int(vals[1])+4*int(vals[2])+5*int(vals[3])+6*int(vals[4])+7*int(vals[5])+8*int(vals[6])+9*int(vals[7])+2*int(vals[8])+3*int(vals[9])+4*int(vals[10])+5*int(vals[11])) % 11))==int(vals[12]);
    
 def isRRN(message) : 
     s = input(message) ;
@@ -144,3 +152,4 @@ def isRRN(message) :
         (front,mid,back) = s.partition("-");
     return s;
 
+#run function here
