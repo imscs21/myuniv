35ef9ec5fc5d3b9f57165810ed9b94d806660c38
hsh97Erica <imscs21@hanyang.ac.kr>
2016-09-07 23:43:35 +0900

    add

---
+++ 2학기/ProgramMethodlogy/PSulGISilSup/src/f/F9.java
@@ -0,0 +1,22 @@
+package f;
+
+import base.BaseMain;
+
+public class F9 extends BaseMain {
+	public static void main(String...strings){
+		int month=getInput().readInt("월? "),day=getInput().readInt("일? "),day_count=0;
+	int[] monthdays = {-1,31,28,31,30,31,30,31,31,30,31,30,31};
+		if(!(
+				(month==1&&day>0&&day<=31)||(month==3&&day>0&&day<=31)||(month==5&&day>0&&day<=31)||(month==7&&day>0&&day<=31)||(month==8&&day>0&&day<=31)||(month==10&&day>0&&day<=31)||(month==12&&day>0&&day<=31)||
+				(month==4&&day>0&&day<31)||(month==6&&day>0&&day<31)||(month==9&&day>0&&day<31)||
+				(month==2&&day>0&&day<=28)
+				)){
+			printline("잘못 입력하셨습니다.");
+		}
+	else{for(int i=1;i<month;i++){
+			day_count+=monthdays[i];
+		}
+		day_count+=day;
+		printline("이 날짜는 1년 중 "+day_count+" 번째 날에 해당됩니다.");}
+	}
+}
