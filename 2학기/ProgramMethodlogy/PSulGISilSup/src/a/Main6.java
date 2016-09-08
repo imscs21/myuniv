fdddd164de37c37c010dd3467b87b0bae6dd2821
hsh97Erica <imscs21@hanyang.ac.kr>
2016-09-08 09:42:49 +0900

    fix

--- 2학기/ProgramMethodlogy/PSulGISilSup/src/a/Main6.java
+++ 2학기/ProgramMethodlogy/PSulGISilSup/src/a/Main6.java
@@ -4,17 +4,12 @@ import java.util.*;
 import base.BaseMain;
 public class Main6 extends BaseMain{
 public static void main(String...strings ){
-	Scanner sc = getScanner();
-	int kor,eng,math,total;
-	int[] d = new int[4];
-	String[] str = {"국어 ","영어 ","수학 "};
-for(int i =0;i<str.length;i++){
-	print(str[i]+"점수를 입력하세요. ");
-	d[i] = sc.nextInt();
-	d[3]+=d[i];
-}
-double average = (d[3]+0.0f)/str.length;
-printline("입력하신 총점은 "+d[3]+" 이고,");
+	int kor=getInput().readInt("국어점수를 입력하세요. "),eng=getInput().readInt("영어점수를 입력하세요. "),math=getInput().readInt("수학점수를 입력하세요. "),total=0;
+	
+
+
+double average = ((total=kor+math+eng)+0.0f)/3;
+printline("입력하신 총점은 "+total+" 이고,");
 printline("평균은 "+Math.round( average*10)/10.0f+" 입니다.");
 	
 }
